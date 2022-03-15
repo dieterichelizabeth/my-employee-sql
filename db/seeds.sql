edab881d056b2a449f99c1ee11e367b2e06ce82a
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-03-15 15:25:09 -0500

    Adds department and roles seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,14 +3,22 @@
 --        ("Marketing"),
 --        ("Floral"),
 --        ("Delivery");
+
 -- See the departments table SELECT * FROM department;
 
 -- tells role belongs to which department
-INSERT INTO roles (title, salary, department_id)
-VALUES ("Lead Social Media Coordinator", 20000.75, 1),
-       ("Social Media Marketing Intern", 12800.00, 2),
-       ("Salesman", 15975.00, 1),
-       ("Bouquet Arranger", 12450.00, 3),
-       ("Floral Designer", 13780.00, 3),
-       ("Head Florist", 22000.00, 3),
-       ("Delivery Driver", 14000.00, 4);
+-- INSERT INTO roles (title, salary, department_id)
+-- VALUES ("Lead Social Media Coordinator", 20000.75, 1),
+--        ("Social Media Marketing Intern", 12800.00, 2),
+--        ("Salesman", 15975.00, 1),
+--        ("Bouquet Arranger", 12450.00, 3),
+--        ("Floral Designer", 13780.00, 3),
+--        ("Head Florist", 22000.00, 3),
+--        ("Delivery Driver", 14000.00, 4);
+
+-- See the roles table --
+-- SELECT * FROM roles;
+-- See the department by searching for "role" --
+-- SELECT roles.id, roles.title, roles.salary, department.department_name
+    -- FROM roles
+    -- INNER JOIN department ON roles.department_id = department.id;
