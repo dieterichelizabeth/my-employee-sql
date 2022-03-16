1e4382296a2a93bb359bbcbc1455a91a097717d4
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-03-15 21:15:23 -0500

    Adds employee table and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -13,11 +13,17 @@ CREATE TABLE roles (
   ON DELETE SET NULL
 );
 
--- CREATE TABLE employee (
---   id INTEGER AUTO_INCREMENT PRIMARY KEY,
---   first_name VARCHAR(30) NOT NULL,
---   last_name VARCHAR(30) NOT NULL,
---   role_id (foreign key to employee role)
---   manager_id (foreign key to refernece another employee that is the manager, null allowed)
--- );
+CREATE TABLE employee (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER,
+  manager_id INTEGER NULL,
+  FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    ON DELETE SET NULL,
+  FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
+);
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,20 +1,20 @@
--- INSERT INTO department (department_name)
--- VALUES ("Sales"),
---        ("Marketing"),
---        ("Floral"),
---        ("Delivery");
+INSERT INTO department (department_name)
+VALUES ("Sales"),
+       ("Marketing"),
+       ("Floral"),
+       ("Delivery");
 
 -- See the departments table SELECT * FROM department;
 
 -- tells role belongs to which department
--- INSERT INTO roles (title, salary, department_id)
--- VALUES ("Lead Social Media Coordinator", 20000.75, 1),
---        ("Social Media Marketing Intern", 12800.00, 2),
---        ("Salesman", 15975.00, 1),
---        ("Bouquet Arranger", 12450.00, 3),
---        ("Floral Designer", 13780.00, 3),
---        ("Head Florist", 22000.00, 3),
---        ("Delivery Driver", 14000.00, 4);
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Lead Social Media Coordinator", 20000.75, 1),
+       ("Social Media Marketing Intern", 12800.00, 2),
+       ("Salesman", 15975.00, 1),
+       ("Bouquet Arranger", 12450.00, 3),
+       ("Floral Designer", 13780.00, 3),
+       ("Head Florist", 22000.00, 3),
+       ("Delivery Driver", 14000.00, 4);
 
 -- See the roles table --
 -- SELECT * FROM roles;
@@ -22,3 +22,36 @@
 -- SELECT roles.id, roles.title, roles.salary, department.department_name
     -- FROM roles
     -- INNER JOIN department ON roles.department_id = department.id;
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Sammy", "Sunflower", 16),
+       ("Tammy", "Tulip", 15),
+       ("Ivy", "Jones", 18),
+       ("Chris", "Cornflower", 21),
+       ("Ally", "Anemone", 19),
+       ("Brick", "Begonia", 17),
+       ("Heather", "Gardenia", 20);
+
+UPDATE employee
+SET manager_id = 9
+WHERE employee.id = 8;
+
+UPDATE employee
+SET manager_id = 14
+WHERE employee.id = 10;
+
+UPDATE employee
+SET manager_id = 14
+WHERE employee.id = 12;
+
+-- See the employee table --
+-- SELECT * FROM employee
+-- See the employee table with required values: currently gets 54 (4 X 14)
+SELECT employee.id, employee.first_name, employee.last_name, roles.title, department.department_name, roles.salary, employee.manager_id
+FROM employee
+    LEFT JOIN roles 
+        ON employee.role_id = roles.id
+    LEFT JOIN department 
+        ON roles.department_id = department.id;
+    LEFT JOIN employee 
+        ON employee.manager_id = employee.id;
