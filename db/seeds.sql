50edc68ec49daacb7b7f22a2dd5db16cc303ca56
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-03-20 22:37:12 -0500

    Query for sum of roles per department

--- db/seeds.sql
+++ db/seeds.sql
@@ -23,7 +23,19 @@ VALUES ("Chris", "Cornflower", 1, NULL),
        ("Ally", "Anemone", 6, 3),
        ("Brick", "Begonia", 7, 2);
 
+--- DISPLAY View the total utilized budget of a department—in other words, the combined salaries of all employees in that department. --
+SELECT department.department_name, SUM(roles.salary)
+FROM employee
+INNER JOIN roles
+    ON employee.role_id = roles.id
+LEFT JOIN department 
+    ON roles.department_id = department.id
+ORDER BY department.department_name;
 
+SELECT department.department_name, SUM(roles.salary)
+FROM department
+INNER JOIN roles ON roles.department_id = department.id
+GROUP BY department.department_name;
 
         -- DISPLAY DEPARTMENTS --
 -- SELECT * FROM department;
